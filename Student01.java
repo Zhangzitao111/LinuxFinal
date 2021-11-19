1866402473c0da4286317f9ef9ae31853a5d9413
zztnb <1253445185@qq.com>
2021-11-19 15:30:21 +0800

    my Final test2

---
+++ Student01.java
@@ -0,0 +1,10 @@
+public class Student01{
+  public int id;
+  public String name;
+  public int age;
+  public Student01(int id,String name,int age){
+    this.id=id;
+    this.name=name;
+    this.age=age;
+  }
+}
