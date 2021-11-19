d204b11386438e7f6e6db1775e503dcd7c07a4a6
zztnb <1253445185@qq.com>
2021-11-19 21:42:23 +0800

    my Final test2.1

---
+++ Notepad.java
@@ -0,0 +1,17 @@
+public class Notepad{
+  public String content;
+  public String time;
+  public int id; 
+  public void setNotepadContent(String content) {
+    this.content = content; 
+}
+  public void setNotepadTime(String time){
+    this.time = time;
+}
+  public void setId(int id){
+    this.id = id;
+}
+
+}
+
+
